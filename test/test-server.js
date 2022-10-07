57ce4d6b654bdf4bf9b5114c186e2fa610556256
Gabriel <gflores@gmail.com>
2022-10-07 18:16:31 +0000

    fixing test folder

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to TeamSTRANGE DEVELOPERS application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to Team STRANGE DEVELOPERS application</h1>"));
         return done();
       });
 
